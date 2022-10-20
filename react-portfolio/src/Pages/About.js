a6cb10cd38483cb8c523b2c4c90acc93703f007f
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-10-20 10:15:33 -0400

    my about is now all set up

--- react-portfolio/src/Pages/About.js
+++ react-portfolio/src/Pages/About.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const About = props => {
   return (
@@ -9,4 +9,4 @@ const About = props => {
 
 About.propTypes= {}
 
-export default About
+export default About;
