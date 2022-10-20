620e0bef8d11a31331c4dfdd139e4e16a608bd3e
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-10-20 03:54:34 -0400

    just put all my current page in for all the pages i created

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,27 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Resume from './Pages/Resume';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [currentPage, setCurrentPage] = useState
+ ('About');
+
+ if (currentPage === 'About') {
+  <About />
+ }
+ if (currentPage === 'Contact') {
+  <Contact />
+ }
+ if (currentPage === 'Portfolio') {
+  <Portfolio />
+ }
+ if (currentPage === 'Resume') {
+  <Resume />
+ }
 }
 
 export default App;
